bac0e43ae15d1aa2e529c2e7c52e35bfc8379f15
amz202 <abdulmajidzeeshan4@gmail.com>
2025-06-22 17:05:30 +0500

    update

--- data/db,js.js
+++ data/db,js.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 
+const MONGO_PW = process.env.MONGO_PW
+
 export const connectDB = async () => {
-    const mongo_uri = "mongodb+srv://abdulmajidzeeshan4:fmJU2xBvwJcF5FHx@cluster0.3spdwnn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    const mongo_uri = `mongodb+srv://abdulmajidzeeshan4:${MONGO_PW}@cluster0.3spdwnn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
     await mongoose.connect(mongo_uri).then(()=>{
         console.log("Connected to MongoDB")
